a157f5c070294c24ac1a0fca0cd85ced1b6b577f
andrefmuller <andrefredericomuller@gmail.com>
2021-10-30 22:35:20 -0300

    Aula 11, 12 e exercicios até o ex015.
    Aula 11, 12 e exercicios até o ex015.

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+console.log ('O console funcionou corretamente!')
+var vel = 90
+console.log (`A velocidade do carro é de ${vel}km/h.`)
+if (vel > 80) {//<-- condição simples 
+    console.log(`Você está acima da velocidade permitida. MULTADO`)
+}
+else {
+    console.log(`Dirija sempre com cuidado. Boa viagem!`)
+}

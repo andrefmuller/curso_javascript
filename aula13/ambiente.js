21fbe9a39c3e09311cf23be3ce857e5a924ead68
andrefmuller <andrefredericomuller@gmail.com>
2021-11-03 13:57:41 -0300

    Atualizações 03/11/2021

---
+++ aula13/ambiente.js
@@ -0,0 +1,12 @@
+cont = 1
+cont2 = 1
+console.log('Tudo bem?')
+while (cont <= 20){
+    console.log(`Tudo bem? Passo ${cont}`  )
+    cont += 1
+}
+
+do {
+    console.log(`Tudo bem? Passo ${cont2}`)
+    cont2 += 1
+} while (cont2 <= 20)
---
+++ aula14/ambiente.js
@@ -0,0 +1,13 @@
+/*
+var c = 1
+while (c <= 5) {
+    console.log(`Oi. Passo ${c}. `)
+    c += 1
+}
+
+*/
+console.log('Vai começar...')
+for (c = 1;c <=10;c += 1) {
+    console.log(`Bora! passo ${c}`)
+}
+console.log('FIM.')
